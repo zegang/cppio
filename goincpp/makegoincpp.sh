2ea395df4e9481cdb3bc90a9a185d1d2387bc5e7
zegang <zegang.luo@qq.com>
2025-01-22 19:02:26 +0800

    refer to goincpp repo

--- goincpp/makegoincpp.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker build -t goincpp .
-docker run -v .:/goincpp -it --rm goincpp:latest
-# cmake -S . -B build;cmake --build build
--- goincpp/src/errors/errors.cpp
+++
@@ -1,8 +0,0 @@
-#include "errors.hpp"
-
-using namespace goincpp::errors;
-
-Error
-newError(const std::string& message) {
-    return std::make_shared<ErrorString>(message);
-}
